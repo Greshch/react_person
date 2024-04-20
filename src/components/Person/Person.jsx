a3bef17e32056878a09d3c119431918b2646a45a
Greshch <greshchook@gmail.com>
2024-04-20 15:29:34 +0300

    2

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,18 @@
-// export const Person = ({ person }) => ();
+import React from 'react';
+import './Person.scss';
+
+const Person = ({ person }) => {
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      <p className="Person__age">I am {person.age}</p>
+      <p className="Person__partner">
+        {person.isMarried
+          ? `${person.partnerName} is my partner`
+          : 'I am not married'}
+      </p>
+    </section>
+  );
+};
+
+export default Person;
